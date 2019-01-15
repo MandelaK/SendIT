11a762effc230ff801dc26a2a6e790eb1a9f717b
MandelaK <kazi.keith@gmail.com>
2019-01-15 15:44:52 +0300

    feature(UI): create a dynamic navbar function and implement it in the homepage
    write function that creates a dynamic navigation bar and implemented it in the index page
    
    [starts #163223117]

---
+++ UI/static/scripts/navbar.js
@@ -0,0 +1,20 @@
+function createNav() {
+    if (window.localStorage.getItem('email') == null) {
+        document.getElementById('logstatus').innerHTML = 'Log In';
+        document.getElementById('logstatus').setAttribute('href', 'login.html');
+        document.getElementById('logstatusb').innerHTML = 'Sign Up';
+        document.getElementById('logstatusb').setAttribute('href', 'register.html');
+    }
+
+    else {
+        document.getElementById('logstatusb').innerHTML = 'Log Out';
+        document.getElementById('logstatusb').setAttribute('href', '#');
+        document.getElementById('logstatusb').setAttribute('onclick', 'logout()');
+        document.getElementById('prof-status').innerHTML = 'Profile';
+        document.getElementById('prof-status').setAttribute('href', 'profile.html');
+        document.getElementById('create-status').innerHTML = 'Create Order';
+        document.getElementById('create-status').setAttribute('href', 'create.html');
+    }
+}
+
+createNav();
