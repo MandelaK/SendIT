c2248cf9c25991f72750148a11b8fc62f0ca4c3f
MandelaK <kazi.keith@gmail.com>
2019-01-15 22:58:36 +0300

    feat(login): users can now log in
    users are able to log in if they already have created an account. They will be redirected if the attempt is successful or notified of any errors otherwise. This page can only be viewed by users who are logged out.
    
    [#163144491]

---
+++ UI/static/scripts/login.js
@@ -0,0 +1,58 @@
+const url = 'https://sendit-api-v2-keith.herokuapp.com/api/v2/auth/login';
+
+if (window.localStorage.getItem('email') == null) {
+        document.getElementById('logstatus').innerHTML = 'Log In';
+        document.getElementById('logstatus').setAttribute('href', 'login.html');
+        document.getElementById('logstatusb').innerHTML = 'Sign Up';
+        document.getElementById('logstatusb').setAttribute('href', 'register.html');
+
+    }
+
+let login = document.getElementById('login');
+
+if (window.localStorage.getItem('email') || window.localStorage.getItem('token')) {
+    redirect: window.location.replace('profile.html');
+}
+login.onclick = function(e) {
+    e.preventDefault();
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+
+    fetch(url, {
+        mode: 'cors',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+
+        },
+        body: JSON.stringify(
+            {email: email,
+            password: password})
+    })
+    .then(res => res.json())
+    .then(data => {
+        let success = data['Success'];
+        let error = data['Error'];
+
+        if (success) {
+            window.localStorage.setItem('token', data["token"]);
+            if (data['admin']) {
+                window.localStorage.setItem('is_admin', data['admin']);
+                window.localStorage.setItem('email', data['email']);
+                redirect: window.location.replace('profile.html');
+                console.log(success);
+            }
+            else {redirect: window.location.replace('profile.html');
+            window.localStorage.setItem('email', data['email']);
+                console.log(success);
+        }}
+
+        else if (error) {
+            document.getElementById('error_output').innerHTML = error;
+            console.log(error);
+
+            document.getElementById('email').value = '';
+            document.getElementById('password').value = '';
+        }
+    }
+    )}
