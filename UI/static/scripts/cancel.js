08257f540bda61643616da90af2a0a0f8d503edc
MandelaK <kazi.keith@gmail.com>
2019-02-05 07:15:36 +0300

    chore(improve feedback): Improve how feedback is displayed to users and admins
    All feedback from the application is not focused whenever it's generated. After successful completion of tasks, users and admins are redirected to their profile pages. Error messages are displayed in red and success messages are displayed in green. Before users and admins can complete changing details of an order, a prompt will ask for confirmation and only execute if the users and admins confirm.
    
    [finishes #163717837]

--- UI/static/scripts/cancel.js
+++ UI/static/scripts/cancel.js
@@ -1,7 +1,9 @@
 'use strict';
 function cancelOrder() {
+    let confirmation = confirm('Are you sure you want to cancel the order?');
     let parcel_id = window.location.search.split('parcel_id=')[1];
     const url = 'https://sendit-api-v2-keith.herokuapp.com/api/v2/parcels/' + parcel_id + '/cancel';
+    if (confirmation){
     fetch(url, {
         method: 'PUT',
         headers: {
@@ -16,12 +18,18 @@ function cancelOrder() {
         let error = data['Error'];
 
         if (success) {
-            document.getElementById('success-feedback').innerHTML = success;
+            document.getElementById('error-output').innerHTML = '';
+            document.getElementById('success-feedback').innerHTML = success + '. You will be redirected to your profile in 5 seconds.';
+            window.scrollBy(0, -window.innerHeight);
+            setTimeout(() => {
+                redirect: window.location.replace('profile.html');}, 5000);
         }
 
         else {
-            document.getElementById('error-feedback').innerHTML = error;
+            document.getElementById('success-feedback').innerHTML = '';
+            document.getElementById('error-output').innerHTML = error;
+            window.scrollBy(0, -window.innerHeight);
         }
     });
 
-}
+}}
